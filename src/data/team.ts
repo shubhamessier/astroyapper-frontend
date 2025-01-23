78aab8af2e69f4bebb3c888281c8c10fbed68e7d
shubhamessier <shubham.gaur7116@gmail.com>
2025-01-23 21:19:22 +0530

    updated team

--- src/data/team.ts
+++ src/data/team.ts
@@ -11,21 +11,22 @@ export const team: TeamMember[] = [
     name: "Somesh",
     role: "Co-Founder",
     bio: "Professional Yapper with a knack for astrology and charts. Firmly believes his hair is on a secret mission to make a mircalous comeback. Stay Tuned!",
-    avatarUrl: "./assets/white-boy-with-black-curls-hair.png",
+    avatarUrl: "https://i.ibb.co/CWWTwSB/white-boy-with-black-curls-hair.png",
     avatarFallback: "Somesh Tangde",
   },
   {
     name: "Shubham",
     role: "Co-Founder",
     bio: "Doesn't Yap much, but when he does, it's worth it. Lives on iced tea and americanos.",
-    avatarUrl: "./assets/white-boy-black-hair-black-hoodie-with-glasses.png",
+    avatarUrl:
+      "https://i.ibb.co/5KdJg9Q/white-boy-black-hair-black-hoodie-with-glasses.png",
     avatarFallback: "Shubham Gaur",
   },
   {
     name: "Akshata",
     role: "UI Design",
     bio: "Code Ninja with a cosmic twist. Believes in astrology, yaps to her friends like it's her full time job and swears to star gods for her debugging skills.",
-    avatarUrl: "./assets/akshata.jpg",
+    avatarUrl: "https://i.ibb.co/1zr2yKx/akshata.jpg",
     avatarFallback: "Akshata",
   },
 ];
